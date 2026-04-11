1b422f1bd748c7abad3bf447c694392c3a481f20
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-11 11:28:01 -0700

    feat: export single-spa lifecycles for angular bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
+import { enableProdMode, NgZone } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Router, NavigationStart } from '@angular/router';
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (false) {
+  enableProdMode();
+}
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  Router,
+  NavigationStart,
+  NgZone,
+});
+
+export const { bootstrap, mount, unmount } = lifecycles;
