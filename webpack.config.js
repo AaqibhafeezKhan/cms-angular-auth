7ac8b093e90a816d1e22c3c051308cf9cb504d5a
Aaqibhafeezkhan <aaqibhafeezkhan@gmail.com>
2026-04-15 21:04:16 +0530

    build: rewrite webpack configuration for SystemJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,43 @@
-const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/main.ts',
   output: {
-    publicPath: "http://localhost:9003/",
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'system',
+    publicPath: process.env.NODE_ENV === 'production' ? '/cms-angular-auth/' : '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: false,
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
-    new ModuleFederationPlugin({
-      name: "cmsAngularAuth",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./AuthComponent": "./src/app/app.component.ts",
-      },
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: false,
     }),
   ],
+  devServer: {
+    port: 8084,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+  externals: {
+    'zone.js': 'Zone',
+  },
 };
